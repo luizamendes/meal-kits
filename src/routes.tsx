2f6ee3b372f088b11f5cc8ad744b493e9b8bf2e3
Luiza Mendes <luizaleitemendes@gmail.com>
2021-10-29 18:57:33 -0300

    Setting react router dom

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { Orders } from "./pages/Orders";
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/orders" exact component={Orders} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
