1ba5218f1f8884a7d12cfc98a78f2fa5d966f7c7
Luiza Mendes <luizaleitemendes@gmail.com>
2021-10-30 17:30:11 -0300

    Fixes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,7 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("Renders welcome greeting", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Welcome/i)).toBeInTheDocument();
 });
