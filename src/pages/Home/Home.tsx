9694028760154e01bb8cdf0c324e72c74cfd12ef
Luiza Mendes <luizaleitemendes@gmail.com>
2021-10-30 15:15:00 -0300

    Including homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,18 @@
+import { useHistory } from "react-router-dom";
+import { Button } from "../../components";
+import style from "./Home.module.scss";
+
 export const Home = () => {
-  return <h1>Home</h1>;
+  const history = useHistory();
+
+  const navigate = () => {
+    history.push("/orders");
+  };
+
+  return (
+    <div className={style.home}>
+      <h1>Welcome!</h1>
+      <Button onClick={navigate}>Scan orders</Button>
+    </div>
+  );
 };
